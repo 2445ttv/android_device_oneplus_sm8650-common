3af68d21fecffe6ab740673fecc32ef6f0aa5a21
2445ttv <118527955+2445ttv@users.noreply.github.com>
2026-08-30 11:41:57 +0800

    Delete bypass_charging/bypass_charging directory

--- bypass_charging/bypass_charging/bypasscharging.mk
+++
@@ -1,5 +0,0 @@
-PRODUCT_PACKAGES += \
-    BypassChargingTile
-
-# SEPolicy
-include device/oneplus/sm8650-common/bypass_charging/sepolicy/SEPolicy.mk
--- bypass_charging/bypass_charging/sepolicy/SEPolicy.mk
+++
@@ -1,2 +0,0 @@
-BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/oneplus/sm8650-common/bypass_charging/sepolicy/vendor
